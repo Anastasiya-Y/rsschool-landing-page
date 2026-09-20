212707e5e76589779a0569c34b1524b62a19d511
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2026-09-20 17:50:49 +0300

    feat: add dark theme

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,8 @@
 import '../sass/style.scss';
 import {setMenu} from './modules/set-burger-menu';
+import {setTheme} from './modules/set-theme';
 
 window.onload = () => {
+  setTheme();
   setMenu();
 };
